68f897e24fe385fc993ebff9a176fffbebbed2a3
effects67 <32003168+effects67@users.noreply.github.com>
2018-09-26 16:17:34 -0500

    .

---
+++ nodequiz/server/helpers/config.js
@@ -0,0 +1,23 @@
+let config = {};
+
+/**
+ * Localhost web server configurations
+ */
+config.web = {};
+config.web.port = process.env.PORT || '3000';
+config.web.secret = 'topsecret';
+
+
+/**
+ * Development database configurations
+ *
+ */
+config.database = {};
+config.database.username = 'gjvance';
+config.database.password = 'password1';
+config.database.port = '15753';
+config.database.url = 'ds15753.mlab.com';
+config.database.name = 'nodequiz';
+
+
+module.exports = config;
