6e5c766bfe59a4409b25e1a21062f49a8b9f2a19
effects67 <32003168+effects67@users.noreply.github.com>
2018-10-08 18:56:41 -0500

    .
    .

--- nodequiz/src/app/app-routing.module.ts
+++ nodequiz/src/app/app-routing.module.ts
@@ -4,10 +4,19 @@ import {RouterModule, Routes} from "@angular/router";
 import {LoginComponent} from "./login/login.component";
 import {SlideComponent} from "./slide/slide.component";
 
+// quizzes
+
+import { QuizCiComponent } from './quiz-ci/quiz-ci.component';
+import { QuizMicroComponent } from './quiz-micro/quiz-micro.component';
+import { QuizTechComponent } from './quiz-tech/quiz-tech.component';
+
 
 const routes: Routes = [
   { path: '', component: LoginComponent },
   { path: 'slide', component: SlideComponent },
+  { path: 'quiz_ci', component: QuizCiComponent },
+  { path: 'quiz_stream', component: QuizTechComponent },
+  { path: 'quiz_micro', component: QuizMicroComponent },
 ];
 
 @NgModule({
